6a59c68957ac7be876bee0c41e1859bb8d005db3
Sebastien Gadois <sgadois@gmail.com>
2015-03-19 13:54:24 +0100

    Add functions for the interface IFrame

--- src/main/java/dcll/SgadRmal/interfaces/IFrame.java
+++ src/main/java/dcll/SgadRmal/interfaces/IFrame.java
@@ -1,7 +1,11 @@
 package dcll.SgadRmal.interfaces;
 
+import dcll.SgadRmal.implementation.Throw;
+
 /**
  * Created by seb on 19/03/15.
  */
 public interface IFrame {
+    public void addThrow(Throw t);
+    public int computeScore();
 }
