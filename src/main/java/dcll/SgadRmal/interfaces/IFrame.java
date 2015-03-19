736e1ff6d8934cea5dabd2d3dc3b5209ebe03407
Sebastien Gadois <sgadois@gmail.com>
2015-03-19 14:35:55 +0100

    Correct checkstyle errors #6

--- src/main/java/dcll/SgadRmal/interfaces/IFrame.java
+++ src/main/java/dcll/SgadRmal/interfaces/IFrame.java
@@ -6,7 +6,16 @@ import dcll.SgadRmal.implementation.Throw;
  * Created by seb on 19/03/15.
  */
 public interface IFrame {
-    public void addThrow(Throw t);
-    public int computeScore();
+    /**
+     * Add a Throw to the game.
+     * @param t     Throw that will be added
+     */
+    void addThrow(Throw t);
+
+    /**
+     * Calculate the score of a bowling game.
+     * @return  Score
+     */
+    int computeScore();
 }
 
