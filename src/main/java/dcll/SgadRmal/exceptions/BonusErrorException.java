703276b30a8b479edf6cc1d9d7ac804e937f2db4
Romain <romain.malet@yahoo.fr>
2015-03-25 16:40:39 +0100

    Add Exception class BonusError Start implement FrameTest with Mock

---
+++ src/main/java/dcll/SgadRmal/exceptions/BonusErrorException.java
@@ -0,0 +1,9 @@
+package dcll.SgadRmal.exceptions;
+
+/**
+ * Created by Romain-toshiba on 25/03/2015.
+ */
+public class BonusErrorException extends Exception {
+
+
+}
--- src/test/java/dcll/SgadRmal/FrameTest.java
+++ src/test/java/dcll/SgadRmal/FrameTest.java
@@ -1,7 +1,37 @@
 package dcll.SgadRmal;
 
+import dcll.SgadRmal.exceptions.BonusErrorException;
+import dcll.SgadRmal.implementation.BonusThrow;
+import dcll.SgadRmal.implementation.Frame;
+import dcll.SgadRmal.implementation.Throw;
+import dcll.SgadRmal.interfaces.IThrow;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
 /**
  * Created by seb on 25/03/15.
  */
 public class FrameTest {
+
+    private Frame jeu;
+    @Mock
+    private IThrow lancer;
+    @Mock
+    private IThrow bonus;
+
+    @Before
+    public void setUp() {
+        lancer = Mockito.mock(Throw.class);
+        bonus = Mockito.mock(BonusThrow.class);
+        jeu = new Frame();
+    }
+
+    @Test(expected=BonusErrorException.class)
+    public void testAddBonusThrowIllegal() {
+        jeu.addThrow(bonus);
+    }
+
+
 }
