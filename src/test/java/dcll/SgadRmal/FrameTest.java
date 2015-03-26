74c33f6f0949f6af5d2b8cb6c1cfe9742428a25a
Romain <romain.malet@yahoo.fr>
2015-03-26 23:54:47 +0100

    #17 Complete and adapt FrameTest

--- src/test/java/dcll/SgadRmal/FrameTest.java
+++ src/test/java/dcll/SgadRmal/FrameTest.java
@@ -1,19 +1,18 @@
 package dcll.SgadRmal;
 
 import dcll.SgadRmal.exceptions.BonusErrorException;
-import dcll.SgadRmal.implementation.LastThrow;
 import dcll.SgadRmal.implementation.Frame;
 import dcll.SgadRmal.implementation.Throw;
+import dcll.SgadRmal.interfaces.ILastThrow;
 import dcll.SgadRmal.interfaces.IThrow;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by Romain on 25/03/15.
@@ -24,61 +23,67 @@ public class FrameTest {
     @Mock
     private IThrow lancer;
     @Mock
-    private IThrow bonus;
+    private ILastThrow lastLancer;
 
     @Before
     public void setUp() {
+        MockitoAnnotations.initMocks(this);
         jeu = new Frame();
     }
 
-    @Test(expected=BonusErrorException.class)
-    public void testAddThrowBonusIllegal() {
-        bonus = Mockito.mock(LastThrow.class);
-        jeu.addThrow(bonus);
+    @Test (expected = BonusErrorException.class)
+    public void testAddLastThrowIllegalFirstThrow() {
+        jeu.addLastThrow(lastLancer);
     }
 
-    @Test(expected=BonusErrorException.class)
-    public void testAddThrowBonusNotAllowed() {
-        bonus = Mockito.mock(LastThrow.class);
-        lancer = Mockito.mock(Throw.class);
-        when(lancer.getFirst()).thenReturn(4);
-        when(lancer.getSecond()).thenReturn(5);
-        for (int i=0; i<9; ++i) {
+    @Test (expected = BonusErrorException.class)
+    public void testAddLastThrowIllegalEighthThrow() {
+        for (int i=0; i<8; ++i) {
             Throw unLancer = Mockito.mock(Throw.class);
             jeu.addThrow(unLancer);
         }
-        jeu.addThrow(lancer);
-        jeu.addThrow(bonus);
+        jeu.addLastThrow(lastLancer);
     }
 
     @Test
     public void testComputeScoreNoBonus() {
+        when(lastLancer.getFirst()).thenReturn(4);
+        when(lastLancer.getSecond()).thenReturn(4);
         int finalScore = 80;
-        for (int i=0; i<10; ++i) {
+        for (int i=0; i<9; ++i) {
             Throw unLancer = Mockito.mock(Throw.class);
             when(unLancer.getFirst()).thenReturn(4);
             when(unLancer.getSecond()).thenReturn(4);
             jeu.addThrow(unLancer);
         }
-        assertEquals(finalScore, jeu.computeScore());
+        jeu.addLastThrow(lastLancer);
+        int testScore = jeu.computeScore();
+
+        verify(lastLancer.getThird(), never());
+        assertEquals(finalScore, testScore);
     }
 
     @Test
     public void testComputeScoreMaxScore() {
+        when(lastLancer.getFirst()).thenReturn(10);
+        when(lastLancer.getSecond()).thenReturn(10);
+        when(lastLancer.getThird()).thenReturn(10);
         int finalScore = 300;
-        for (int i=0; i<10; ++i) {
+        for (int i=0; i<9; ++i) {
             Throw strike = Mockito.mock(Throw.class);
             when(strike.getFirst()).thenReturn(10);
             jeu.addThrow(strike);
         }
-        bonus = Mockito.mock(LastThrow.class);
-        when(bonus.getFirst()).thenReturn(10);
-        when(bonus.getSecond()).thenReturn(10);
+        jeu.addLastThrow(lastLancer);
+
         assertEquals(finalScore, jeu.computeScore());
     }
 
     @Test
     public void testComputeScoreSpare() {
+        when(lastLancer.getFirst()).thenReturn(5);
+        when(lastLancer.getSecond()).thenReturn(5);
+        when(lastLancer.getThird()).thenReturn(5);
         int finalScore = 150;
         for (int i=0; i<10; ++i) {
             Throw unLancer = Mockito.mock(Throw.class);
@@ -86,9 +91,37 @@ public class FrameTest {
             when(unLancer.getSecond()).thenReturn(5);
             jeu.addThrow(unLancer);
         }
-        bonus = Mockito.mock(LastThrow.class);
-        when(bonus.getFirst()).thenReturn(5);
-        verify(bonus, never()).getSecond();
+        jeu.addLastThrow(lastLancer);
+
+        assertEquals(finalScore, jeu.computeScore());
+    }
+
+    @Test
+    public void testComputeScoreStrikeSpare() {
+        when(lastLancer.getFirst()).thenReturn(5);
+        when(lastLancer.getSecond()).thenReturn(5);
+        when(lastLancer.getThird()).thenReturn(10);
+
+        Throw strike = Mockito.mock(Throw.class);
+        when(strike.getFirst()).thenReturn(10);
+
+        Throw spare = Mockito.mock(Throw.class);
+        when(spare.getFirst()).thenReturn(5);
+        when(spare.getSecond()).thenReturn(5);
+
+        int finalScore = 200;
+
+        jeu.addThrow(strike); // 20
+        jeu.addThrow(spare); // 20
+        jeu.addThrow(strike); // 20
+        jeu.addThrow(spare); // 20
+        jeu.addThrow(strike); // 20
+        jeu.addThrow(spare); // 20
+        jeu.addThrow(strike); // 20
+        jeu.addThrow(spare); // 20
+        jeu.addThrow(strike); // 20
+        jeu.addLastThrow(lastLancer); // 20
+
         assertEquals(finalScore, jeu.computeScore());
     }
 }
