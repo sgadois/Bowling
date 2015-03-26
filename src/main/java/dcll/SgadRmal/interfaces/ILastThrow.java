8b8cceb00c712e59d29453e92305ea7d55fe8cb5
Romain <romain.malet@yahoo.fr>
2015-03-26 20:13:55 +0100

    #15 Add ILastThrow interface

---
+++ src/main/java/dcll/SgadRmal/interfaces/ILastThrow.java
@@ -0,0 +1,19 @@
+package dcll.SgadRmal.interfaces;
+
+/**
+ * Created by Romain on 26/03/2015.
+ */
+public interface ILastThrow {
+
+    /**
+     * Set the score of the third try of the last throw.
+     *
+     * @param score Number of pins knocked over
+     */
+    void setThird(int score);
+
+    /**
+     * @return Score of the third try
+     */
+    int getThird();
+}
