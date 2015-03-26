71b640ebe3e48cc34dbebddd0bdd3e43034c3f7a
Sebastien Gadois <sgadois@gmail.com>
2015-03-27 00:14:48 +0100

    Add LastThrowTest.java which implement the tests for LastThrow. #5

---
+++ src/test/java/dcll/SgadRmal/LastThrowTest.java
@@ -0,0 +1,127 @@
+package dcll.SgadRmal;
+
+import dcll.SgadRmal.exceptions.IncorrectValueForTryException;
+import dcll.SgadRmal.exceptions.SecondTryNotDoneException;
+import dcll.SgadRmal.implementation.LastThrow;
+import dcll.SgadRmal.implementation.ThrowType;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by seb on 26/03/15.
+ */
+public class LastThrowTest {
+
+    private LastThrow lastThrow;
+
+    @Before
+    public void setUp() {
+        lastThrow = new LastThrow();
+    }
+
+
+
+    @Test
+    public void testCreateLastThrow() {
+
+        // Assertions
+        Assert.assertEquals("The value of the first try must be -1", -1, lastThrow.getFirst());
+        Assert.assertEquals("The value of the second try must be -1", -1, lastThrow.getSecond());
+        Assert.assertEquals("The value of the third try must be -1", -1, lastThrow.getThird());
+    }
+
+    @Test
+    public void testStrike() {
+
+        // Setup
+        final int score = 10;
+
+        // Code under tests
+        lastThrow.setFirst(score);
+
+        // Assertions
+        Assert.assertEquals(score, lastThrow.getFirst());
+        Assert.assertEquals(-1, lastThrow.getSecond());
+        Assert.assertEquals(-1, lastThrow.getThird());
+        Assert.assertEquals(ThrowType.STRIKE, lastThrow.getType());
+    }
+
+    @Test
+    public void testSpare() {
+
+        // Setup
+        final int score = 5;
+        lastThrow.setFirst(5);
+
+        // Code under test
+        lastThrow.setSecond(score);
+
+        // Assertions
+        Assert.assertEquals(score, lastThrow.getSecond());
+        Assert.assertEquals(-1, lastThrow.getThird());
+        Assert.assertEquals(ThrowType.SPARE, lastThrow.getThird());
+    }
+
+    @Test
+    public void testNormal() {
+
+        // Setup
+        final int score = 2;
+        lastThrow.setFirst(1);
+
+        // Code under test
+        lastThrow.setSecond(score);
+
+        // Assertions
+        Assert.assertEquals(score, lastThrow.getSecond());
+        Assert.assertEquals(0, lastThrow.getThird());
+        Assert.assertEquals(ThrowType.NORMAL, lastThrow.getThird());
+    }
+
+    @Test(expected = IncorrectValueForTryException.class)
+    public void testSetIncorrectValueSecondTry() {
+
+        // Setup
+        final int score = 51;
+        lastThrow.setFirst(1);
+
+        // Code under test
+        lastThrow.setSecond(score);
+    }
+
+    @Test(expected = IncorrectValueForTryException.class)
+    public void testSetIncorrectValueThirdTry() {
+
+        // Setup
+        final int score = 27;
+        lastThrow.setFirst(10);
+        lastThrow.setSecond(5);
+
+        // Code under test
+        lastThrow.setThird(score);
+    }
+
+    @Test(expected = SecondTryNotDoneException.class)
+    public void testSetThirdBeforeSecond() {
+
+        // Setup
+        final int score = 7;
+
+        // Code under test
+        lastThrow.setThird(score);
+    }
+
+    @Test(expected = IncorrectValueForTryException.class)
+    public void testSetThirdTryInNormalThrow() {
+
+        // Setup
+        final int score = 6;
+        lastThrow.setFirst(1);
+        lastThrow.setSecond(2);
+
+        // Code under
+        lastThrow.setThird(score);
+    }
+
+}
