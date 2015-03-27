489259cb691f0a6ce1e59e8899cf6bd2bb93d481
Romain <romain.malet@yahoo.fr>
2015-03-27 01:10:34 +0100

    #10 Implement Throw class Add throws exceptions for methods

--- src/main/java/dcll/SgadRmal/implementation/Throw.java
+++ src/main/java/dcll/SgadRmal/implementation/Throw.java
@@ -1,34 +1,66 @@
 package dcll.SgadRmal.implementation;
 
+import dcll.SgadRmal.exceptions.FirstTryNotDoneException;
+import dcll.SgadRmal.exceptions.IncorrectValueForTryException;
 import dcll.SgadRmal.interfaces.IThrow;
 
 /**
  * Created by seb on 19/03/15.
+ * @author Romain
+ *
  */
 public class Throw implements IThrow {
 
-    @Override
-    final public void setFirst(int score) {
+    protected int first;
+    protected int second;
+    protected ThrowType type;
+    protected final static int MIN = 0;
+    protected final static int MAX = 10;
 
+    public Throw() {
+        first = -1;
+        second = -1;
+        type = null;
     }
 
     @Override
-    public void setSecond(int score) {
+    final public void setFirst(int score) throws IncorrectValueForTryException {
+        if (score < MIN || score > MAX)
+            throw new IncorrectValueForTryException("Value is incorrect");
+        else {
+            first = score;
+            if (first == MAX)
+                type = ThrowType.STRIKE;
+        }
+    }
 
+    @Override
+    public void setSecond(int score) throws FirstTryNotDoneException, IncorrectValueForTryException {
+        if (first < MIN)
+            throw new FirstTryNotDoneException("First try is not set");
+        else if (type == ThrowType.STRIKE || first + score > MAX)
+            throw new IncorrectValueForTryException("Value is incorrect");
+        else {
+            second = score;
+            if (first + second == MAX)
+                type = ThrowType.SPARE;
+            else
+                type = ThrowType.NORMAL;
+        }
     }
 
     @Override
     final public int getFirst() {
-        return 0;
+        return first;
     }
 
     @Override
     final public int getSecond() {
-        return 0;
+        return second;
     }
 
     @Override
     final public ThrowType getType() {
-        return null;
+        return type;
     }
 }
