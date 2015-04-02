bccbcedbbd72bff64aa4f645bd82fdaa2972cd1f
Romain <romain.malet@yahoo.fr>
2015-04-02 09:49:20 +0200

    #34 Fix checkstyle errors for IFrame

--- src/main/java/dcll/SgadRmal/interfaces/IFrame.java
+++ src/main/java/dcll/SgadRmal/interfaces/IFrame.java
@@ -8,10 +8,12 @@ import dcll.SgadRmal.implementation.Throw;
  * Created by seb on 19/03/15.
  */
 public interface IFrame {
+
     /**
      * Add a Throw to the game.
      *
      * @param t Throw that will be added
+     * @throws InvalidFrameException if throw attempt to add is invalid
      */
     void addThrow(Throw t) throws InvalidFrameException;
 
@@ -19,6 +21,7 @@ public interface IFrame {
      * Add the last throw of the game.
      *
      * @param t The last throw
+     * @throws InvalidFrameException if last throw attempt to add is invalid
      */
     void addLastThrow(LastThrow t) throws InvalidFrameException;
 
@@ -26,6 +29,7 @@ public interface IFrame {
      * Calculate the score of a bowling game.
      *
      * @return Score
+     * @throws InvalidFrameException if attempt to compute incomplete frame
      */
     int computeScore() throws InvalidFrameException;
 }
